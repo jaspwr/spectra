7419c1d5ac1afa4c2c1cad6a902939e8b1fa38e4
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-07-29 17:35:06 +1000

    fixes

---
+++ src/lib/PipelineEditor/InputHandleList.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Handle, Position } from "@xyflow/svelte";
+
+  export let handles: [string, string][];
+  export let top: number;
+</script>
+
+{#each handles as [id, label], i}
+  <Handle
+    type="target"
+    position={Position.Left}
+    style={`top:${top + 6 + 15 * i}px;`}
+    {id}
+  />
+  <div class="label" style={`top:${top + 15 * i}px;`}>{label}</div>
+{/each}
+
+<style>
+  .label {
+    position: absolute;
+    font-size: 10px;
+    margin-left: -2px;
+  }
+</style>
